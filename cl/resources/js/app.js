4d9a2502442a5f25f97360e7e983b734227744db
sanakaddo@hotmail.com <sanakaddo@hotmail.com>
2020-10-14 14:00:28 +0100

    Laravel Setup and Configurations

--- cl/resources/js/app.js
+++ cl/resources/js/app.js
@@ -1,1 +1,16 @@
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import Buefy from 'buefy'
+
+Vue.use(Buefy)
+
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+//May need to create custome components later on
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
